b385a3ba4d0ee305ce1abc6e23749d5ea2845f5d
mnnnguy23 <michaelnguyen1105@gmail.com>
2023-03-30 19:57:50 -0500

    Modified css lua

--- lua/plugins/extra/lang/css.lua
+++ lua/plugins/extra/lang/css.lua
@@ -2,7 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         opts= function(_, opts)
-            vim.list_extend(opts.ensure_installed, { "css", "scss", "less" })
+            vim.list_extend(opts.ensure_installed, { "css", "scss" })
         end,
     },
 
@@ -15,9 +15,7 @@ return {
                         css = {
                             validate = true
                           },
-                          less = {
-                            validate = true
-                          },
+                          
                           scss = {
                             validate = true
                           }
