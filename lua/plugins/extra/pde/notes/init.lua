cee178aa9da580ae4eb91c32e096840724b13114
mnnnguy23 <michaelnguyen1105@gmail.com>
2023-08-27 06:35:17 -0500

    Added more plugins

---
+++ lua/plugins/extra/pde/notes/init.lua
@@ -0,0 +1,7 @@
+return {
+	{ "itchyny/calendar.vim", cmd = { "Calendar" } },
+	{ "folke/twilight.nvim", opts = {}, cmd = { "Twilight", "TwilightEnable", "TwilightDisable" } },
+	{ "folke/zen-mode.nvim", opts = {}, cmd = { "ZenMode" } },
+	{ "dhruvasagar/vim-table-mode", ft = { "markdown", "org", "norg" } },
+	{ "lukas-reineke/headlines.nvim", opts = {}, ft = { "markdown", "org", "norg" } },
+}
